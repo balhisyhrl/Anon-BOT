b698c8796a759a1f9ae46e859e505f14ff4baf6f
balhisyhrl <iqbalhidayatsyahril@gmail.com>
2022-07-19 02:26:01 +0000

    V.1.0.6

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,70 +1,3 @@
-/* const { sticker } = require('../lib/sticker')
-const WSF = require('wa-sticker-formatter')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  let stiker = false
-  let wsf = false
-  try {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/webp/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `balas stiker dengan perintah ${usedPrefix + command}`
-      wsf = new WSF.Sticker(img, {
-        pack: global.packname,
-        author: global.author,
-        crop: false,
-      })
-    } else if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `balas gambar dengan perintah ${usedPrefix + command}`
-      wsf = new WSF.Sticker(img, {
-        pack: global.packname,
-        author: global.author,
-        crop: false,
-      })
-    } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
-      let img = await q.download()
-      if (!img) throw `balas video dengan perintah ${usedPrefix + command}`
-      wsf = new WSF.Sticker(img, {
-        pack: global.packname,
-        author: global.author,
-        crop: true,
-      })
-    } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else throw 'URL tidak valid!'
-    }
-  } catch (e) {
-    throw e
-  }
-  finally {
-    if (wsf) {
-      await wsf.build()
-      const sticBuffer = await wsf.get()
-      if (sticBuffer) await conn.sendMessage(m.chat, { sticker: sticBuffer }, {
-        quoted: m,
-        mimetype: 'image/webp',
-        ephemeralExpiration: 86400
-      })
-    }
-    if (stiker) await conn.sendMessage(m.chat, { sticker: stiker }, {
-      quoted: m,
-      mimetype: 'image/webp',
-      ephemeralExpiration: 86400
-    })
-    // else throw `Gagal${m.isGroup ? ', balas gambarnya!' : ''}`
-  }
-}
-handler.help = ['stiker', 'stiker <url>']
-handler.tags = ['sticker']
-handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
-
-module.exports = handler
-
-const isUrl = (text) => {
-  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
-} */
 const uploadImage = require('../lib/uploadImage')
 const ocrapi = require("ocr-space-api-wrapper")
 const detection = require('../lib/detect')
@@ -82,13 +15,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       let gettext = await getText(url)
       let isBadword = listkatakotor.exec(gettext.toLowerCase())
       if(isBadword) throw `Stiker mengandung kata kata kotor tidak di ijinkan`
-      let { status, nude, nudescore, weapon, weaponscore, alcohol, alcoholscore, drugs, drugsscore, gore, gorescore } = await detection(url)
-      //if(status == false) return 0
-      if(nude == true) throw `Gambar mengandung unsur ketelanjangan tidak di ijinkan\nScore : ${nudescore}`
-      if(weapon == true) throw `Gambar mengandung unsur senjata tidak di ijinkan\nScore : ${weaponscore}`
-      if(alcohol == true) throw `Gambar mengandung unsur minuman keras tidak di ijinkan\nScore : ${alcoholscore}`
-      if(drugs == true) throw `Gambar mengandung unsur obat obatan tidak di ijinkan\nScore : ${drugsscore}`
-      if(gore == true) throw `Gambar mengandung unsur gore tidak di ijinkan\nScore : ${gorescore}`
       let encmedia = await conn.sendImageAsSticker(m.chat, media, m, { packname: global.packname, author: global.author })
       await fs.unlinkSync(encmedia)
   } else if (/video/.test(mime)) {
